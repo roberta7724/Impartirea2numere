5daf3d218ccaf76ef4cb29da11b5dbbc1cc2b023
roberta7724 <roberta.bara@emanuel.ro>
2023-10-16 13:03:16 +0300

    Initial project setup

---
+++ Impartirea2numere/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Impartirea2numere
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int deimpartit = int.Parse(Console.ReadLine());
+            int impartitor = int.Parse(Console.ReadLine());
+
+            if (impartitor != 0)
+            {
+                if (deimpartit % impartitor == 0)
+                {
+                    Console.WriteLine(deimpartit / impartitor);
+                }
+                else
+                {
+                    Console.WriteLine("Impartirea nu se poate efectua in mod exact");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Impartirea la 0 nu este posibila");
+            }
+
+
+        }
+    }
+}
